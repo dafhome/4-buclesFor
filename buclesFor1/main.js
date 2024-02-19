d8f719348eb852eadd8458bdb1adec19b6ff31e8
dafhome <d.fernalvarez@gmail.com>
2024-02-19 20:53:28 +0100

    commit

--- buclesFor1/main.js
+++ buclesFor1/main.js
@@ -1,4 +1,5 @@
 // 1- Imprime los números entre 20 y 50, ambos incluidos, en orden DESCENDENTE
+document.write('Aquí te meuestro los números entre 20 y 50 en orden descendente:<br>');
 
 for (let i=50; i>=20; i--){
     document.write(i+'<br>');
